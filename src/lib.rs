30c6f462989cb8daf25db3d731652b56f6c4d9a8
Pil Eghoff <pil.eghoff@gmail.com>
2023-06-18 19:19:55 +0200

    Refactor slightly

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,11 @@
 #![doc = include_str!("../README.md")]
-pub mod adapter;
 pub mod base_message;
-pub mod client;
 pub mod errors;
 pub mod events;
 #[doc(hidden)]
 mod macros;
 pub mod prelude;
+pub mod protocol_message;
 pub mod requests;
 pub mod responses;
 pub mod reverse_requests;
